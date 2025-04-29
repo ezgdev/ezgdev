b09280a2e069dfdeed383c9e6159d622aaa4b571
EzeG92 <gim24001@byui.edu>
2025-04-29 00:39:25 -0300

    netlify config

--- personal-web/astro.config.mjs
+++ personal-web/astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
+
   }
 });
+
